6876e42c693f18eee8a2f913929ef6b0dfb0a1f3
Hoppebolden12 <115531794+Hoppebolden12@users.noreply.github.com>
2024-04-09 15:27:11 +0200

    b

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import imageviewer.gui.controller.ImageViewController;
 import javafx.application.Application;
+import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
